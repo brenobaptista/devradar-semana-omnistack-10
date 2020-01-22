fed452ce1a502d26a31fa9970d382bc2477e6120
brenobaptista <brenomb07@hotmail.com>
2020-01-22 17:35:12 -0300

    Added Update and Destroy Methods

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -40,12 +40,40 @@ module.exports = {
     return res.json(dev);
   },
 
-  async update() {
-    // atualizar (editar) um único dev
+  async update(req, res) {
+    const { name, bio, avatar_url, techs, latitude, longitude } = req.body;
 
+    let editedDev = await Dev.findById(req.params.id);
+
+    if (techs) {
+      const techsArray = parseStringAsArray(techs);
+      editedDev.techs = techsArray;
+    } else {
+      editedDev.techs = editedDev.techs;
+    }
+
+    if (latitude && longitude) {
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+      }
+      editedDev.location = location;
+    } else {
+      editedDev.location = editedDev.location;
+    }
+    
+    editedDev.name = name || editedDev.name;
+    editedDev.avatar_url = avatar_url || editedDev.avatar_url;
+    editedDev.bio = bio || editedDev.bio;
+
+    const result = await editedDev.save();
+
+    return res.json(result);
   },
 
-  async destroy() {
-    // deletar um dev do banco de dados
+  async destroy(req, res) {
+    const deletedDev = await Dev.findByIdAndDelete(req.params.id);
+
+    return res.json({ message: `Dev deleted: ${deletedDev.name}` })
   },
 };
